2868f98abd84897db806e749e88195f10c56b194
One-percent-athlete <one.percent.athlete@gmail.com>
2024-07-26 10:25:38 +0900

    Add detail page and view

--- wagapp/home/urls.py
+++ wagapp/home/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('<slug:slug>', views.post_detail, name='post_detail'),
 ]
--- wagapp/home/views.py
+++ wagapp/home/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'home/index.html', context)
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug,)
+    context = {"post": post,}
+    return render(request, 'home/detail.html', context)
