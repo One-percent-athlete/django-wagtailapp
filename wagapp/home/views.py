1d4a0fdfa33938f2c6b43088b10774f79c4b0ccb
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-02 00:59:20 +0900

    Add filter function

--- wagapp/home/views.py
+++ wagapp/home/views.py
@@ -2,24 +2,26 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.core.paginator import Paginator
 from taggit.models import Tag
 
-from .models import Post, Review
+from .models import Post, Review,Categories
 from .forms import ReviewForm
 
 def post_list(request):
     posts = Post.objects.all()
     tags = Tag.objects.all()
+    categories = Categories.objects.all()
 
     post_per_page = 3
     paginator = Paginator(posts, post_per_page)
     page_number = request.GET.get('page')
     page_object = paginator.get_page(page_number)
 
-    context = {'posts': posts, 'page_object': page_object, 'tags': tags}
+    context = {'posts': posts, 'page_object': page_object, 'tags': tags, 'categories':categories}
     return render(request, 'home/index.html', context)
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug,)
     tags = Tag.objects.all()
+    categories = Categories.objects.all()
 
     if request.method == 'POST':
         review_form = ReviewForm(request.POST)
@@ -35,6 +37,23 @@ def post_detail(request, slug):
     else:
         review_form = ReviewForm
     
-    context = {'post': post,'review_form':review_form, 'tags': tags}
+    context = {'post': post,'review_form':review_form, 'tags': tags, 'categories':categories}
     return render(request, 'home/detail.html', context)
 
+def post_filter(request, category=None, tag=None):
+    posts = Post.objects.all()
+    categories = Categories.objects.all()
+    tags = Tag.objects.all()
+
+    requested_category = None
+    if category:
+        requested_category = get_object_or_404(Categories, slug=category)
+        posts = posts.filter(category__in = [requested_category])
+
+    post_per_page = 5
+    paginator = Paginator(posts, post_per_page)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+
+    context = {'categories': categories, 'tags': tags, 'page_object':page_object}
+    return render(request, 'home/filters.html', context)
